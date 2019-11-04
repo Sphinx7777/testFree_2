aff59587ecdecca6cf7c3d34762d1949e6a38238
Sphinx7777 <spamoglot13@gmail.com>
2019-11-04 12:13:28 +0200

    stop

--- src/Components/Lists/Lists.jsx
+++ src/Components/Lists/Lists.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import s from './Lists.module.scss';
 import {List} from "./List/List";
+import {Header} from "../Header/Header";
 
 
 export const Lists = ({listsArr1, listsArr2, listsArr3,...props}) => {
 
 
 	return (
+		<>
+			<Header />
 		<div className={s.listsWrapper}>
 			<List
 				listsArray={listsArr1}
@@ -24,6 +27,7 @@ export const Lists = ({listsArr1, listsArr2, listsArr3,...props}) => {
 				{...props}
 			/>
 		</div>
+			</>
 	);
 };
 
